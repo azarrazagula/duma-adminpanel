f26846f9b43ce23e5097ee97d1ac8f60e8adfd21
azarrazagula <azarrazagula@gmail.com>
2026-05-04 18:25:38 +0530

    done

--- src/EndpontsLogics/authService.js
+++ src/EndpontsLogics/authService.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
-import { API_BASE_URL as API_URL } from '../constants/config';
+import axios from "axios";
+import { API_BASE_URL as API_URL } from "../constants/config";
 
 export const loginAdmin = async (email, password) => {
   try {
     const response = await axios.post(`${API_URL}/login`, { email, password });
     if (response.data.token) {
-      localStorage.setItem('adminToken', response.data.token);
-      localStorage.setItem('adminUser', JSON.stringify(response.data));
+      localStorage.setItem("adminToken", response.data.token);
+      localStorage.setItem("adminUser", JSON.stringify(response.data));
     }
     return response.data;
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Login failed');
+    throw new Error(error.response?.data?.message || "Login failed");
   }
 };
 
 export const logoutAdmin = () => {
-  localStorage.removeItem('adminToken');
-  localStorage.removeItem('adminUser');
+  localStorage.removeItem("adminToken");
+  localStorage.removeItem("adminUser");
 };
 
 export const getAdminUser = () => {
-  const user = localStorage.getItem('adminUser');
+  const user = localStorage.getItem("adminUser");
   return user ? JSON.parse(user) : null;
 };
 
 export const isAdminAuthenticated = () => {
-  return !!localStorage.getItem('adminToken');
+  return !!localStorage.getItem("adminToken");
 };
